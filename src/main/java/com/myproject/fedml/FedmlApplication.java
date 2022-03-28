ce9bcf4b41e8b36d6151eb134393ad2603055022
MacroYuan <1140281270@qq.com>
2022-03-28 19:21:45 +0800

    创建项目

---
+++ src/main/java/com/myproject/fedml/FedmlApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.fedml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FedmlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FedmlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/myproject/fedml/FedmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.fedml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FedmlApplicationTests {
+
+//    @Test
+//    void contextLoads() {
+//    }
+
+}
